1b09b1bedc14bed4461a3bd94825bb9bf8e0d7a3
malopolo <48295514+malopolo@users.noreply.github.com>
2021-07-29 12:20:48 +0200

    update 4

--- Calculation/Start_sim/save_.m
+++ Calculation/Start_sim/save_.m
@@ -1,4 +1,4 @@
-function save_(out_,folder,name,Time)
+function save_(out_,folder,batt_soc,strategy,Time)
     out = out_;
-    save(strcat(folder,'\',name,'_',num2str(Time),'.mat'),'out');
+    save(strcat(folder,'\B',string(batt_soc),'_',strategy,'_',num2str(Time),'.mat'),'out');
 end
