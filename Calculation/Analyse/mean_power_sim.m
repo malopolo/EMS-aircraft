55d3086c01bde4cccd0638e70f48e1ff3ffe5ea7
malopolo <48295514+malopolo@users.noreply.github.com>
2021-07-29 13:00:20 +0200

    Update mean_power_sim.m

--- Calculation/Analyse/mean_power_sim.m
+++ Calculation/Analyse/mean_power_sim.m
@@ -1,20 +1,14 @@
 function [P_FC, P_PV, P_batt, P_SC] = mean_power_sim(var,side)
     out = var.out;
     if strcmp(side,'in')
-%         P_PV = mean(out.PV.signals.values(:,1) .* out.PV.signals.values(:,4));
-%         P_FC = mean(out.FC.signals.values(:,1) .* out.FC.signals.values(:,4));
-%         P_batt = mean(out.Batt.signals.values(:,1) .* out.Batt.signals.values(:,3));
         P_PV = mean(out.compar_in.signals.values(:,5));
         P_FC = mean(out.compar_in.signals.values(:,3));
         P_batt = mean(out.compar_in.signals.values(:,4));
     else
-%         P_PV = mean(out.LOAD.signals.values(:,1) .* out.PV.signals.values(:,5));
-%         P_FC = mean(out.LOAD.signals.values(:,1) .* out.FC.signals.values(:,5));
-%         P_batt = mean(out.LOAD.signals.values(:,1) .* out.Batt.signals.values(:,5));
         P_PV = mean(out.compar_out.signals.values(:,5));
         P_FC = mean(out.compar_out.signals.values(:,3));
         P_batt = mean(out.compar_out.signals.values(:,4));
     end
-    % WARNING : Calculated after 0.2sec
+    % WARNING : Calculated after 2e-4sec
     P_SC = mean(out.SC.signals.values(:,1));
 end
