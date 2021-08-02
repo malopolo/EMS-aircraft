49730e5e817918bfedecd307b5a3cfbb3376cec0
malopolo <48295514+malopolo@users.noreply.github.com>
2021-08-03 00:29:38 +0200

    ok

--- Calculation/Analyse/consumption.m
+++ Calculation/Analyse/consumption.m
@@ -1,6 +1,6 @@
 function [col] = consumption(sim)
     sz_sim = size(sim);
-    col = zeros(sz_sim(1),4);
+    col = zeros(sz_sim(1),5);
     for i = 1:sz_sim(1)
         t = sim(i).tout;
         N = 50; % Number of cells
@@ -15,5 +15,7 @@ function [col] = consumption(sim)
         col(i,3) = 1-trapz(t,sim(i).Batt.signals.values(:,3))/15/60/60; % 15 = rated capacity
 
         col(i,4) = 1- sim(i).Batt.signals.values(end,7);
+        
+        col(i,5) = sim(i).real_time_sim;
     end
 end
