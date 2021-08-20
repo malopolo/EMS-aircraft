af870cb21dc502cd4ce126e4b0dc90864a3addf9
malopolo <48295514+malopolo@users.noreply.github.com>
2021-08-20 17:44:24 +0200

    clean up

--- Calculation/dev_GA/slprj/raccel/fast_powertrain/fast_powertrain_private.h
+++
@@ -1,30 +0,0 @@
-#ifndef RTW_HEADER_fast_powertrain_private_h_
-#define RTW_HEADER_fast_powertrain_private_h_
-#include "rtwtypes.h"
-#include "builtin_typeid_types.h"
-#include "multiword_types.h"
-#if !defined(rt_VALIDATE_MEMORY)
-#define rt_VALIDATE_MEMORY(S, ptr)   if(!(ptr)) {\
-  ssSetErrorStatus(rtS, RT_MEMORY_ALLOCATION_ERROR);\
-  }
-#endif
-#if !defined(rt_FREE)
-#if !defined(_WIN32)
-#define rt_FREE(ptr)   if((ptr) != (NULL)) {\
-  free((ptr));\
-  (ptr) = (NULL);\
-  }
-#else
-#define rt_FREE(ptr)   if((ptr) != (NULL)) {\
-  free((void *)(ptr));\
-  (ptr) = (NULL);\
-  }
-#endif
-#endif
-extern real_T look1_binlxpw ( real_T u0 , const real_T bp0 [ ] , const real_T
-table [ ] , uint32_T maxIndex ) ; extern void sfun_spssw_discc_DSS (
-SimStruct * rts ) ;
-#if defined(MULTITASKING)
-#error Model (fast_powertrain) was built in \SingleTasking solver mode, however the MULTITASKING define is \present. If you have multitasking (e.g. -DMT or -DMULTITASKING) \defined on the Code Generation page of Simulation parameter dialog, please \remove it and on the Solver page, select solver mode \MultiTasking. If the Simulation parameter dialog is configured \correctly, please verify that your template makefile is \configured correctly.
-#endif
-#endif
--- Main/slprj/accel/power_FCHPS_MEA/power_FCHPS_MEA.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set MATLAB=G:\Matlab_2b\software
-"%MATLAB%\bin\win64\gmake" -f power_FCHPS_MEA.mk  NON_ANSI_TRIG_FCN=1 OPTS="-DTID01EQ=0"
